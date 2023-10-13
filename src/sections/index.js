f24851adf8d28320fb2da616590701d517160989
Christian De La Cruz <christian2793@gmail.com>
2023-10-12 21:32:12 -0400

    Initial Commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import Subscribe from './Subscribe';
+import CustomerReviews from './CustomerReviews';
+import Footer from './Footer';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer'
+import SuperQuality from './SuperQuality';
+import PopularProducts from './PopularProducts';
+
+export {
+    Hero,
+    Subscribe,
+    CustomerReviews,
+    Footer,
+    Services,
+    SpecialOffer,
+    SuperQuality,
+    PopularProducts
+}
